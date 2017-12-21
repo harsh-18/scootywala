16456fa1d773d9138631dfde7004af6415a510e9
shikharvaish28 <shikarvaish@gmail.com>
2017-12-21 17:42:20 +0530

    started login integration

---
+++ Scootywala-home/js/main.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD0A6IkM01sOL7pLAdew2KV-A4mq7QSSN4",
+    authDomain: "scootywala-dfcac.firebaseapp.com",
+    databaseURL: "https://scootywala-dfcac.firebaseio.com",
+    projectId: "scootywala-dfcac",
+    storageBucket: "scootywala-dfcac.appspot.com",
+    messagingSenderId: "798565631194"
+  };
+  firebase.initializeApp(config);
+
+  //take the booking
+
+  
